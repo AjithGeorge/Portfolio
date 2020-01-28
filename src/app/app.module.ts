ca154ffe4a8b7cf1f23246491d127d587d872b39
Ajith George <Ajith.George@neudesic.com>
2020-01-28 19:26:33 +0530

    Implemented routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,34 @@ import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent} from './portfolio/portfolio.component';
+
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: '',   redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/' },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
